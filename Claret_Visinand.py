af046550afdcbaa0a3dce7de46c9edb34ab679ae
Romain Claret <contact@rocla.ch>
2015-12-17 17:22:23 +0100

    / Kill tabs -> 4 spaces

--- Claret_Visinand.py
+++ Claret_Visinand.py
@@ -3,7 +3,6 @@ import pygame
 from pygame.locals import *
 
 if not pygame.font:
-	print('Warning, fonts disabled')
+    print('Warning, fonts disabled')
 if not pygame.mixer:
-	print('Warning, sound disabled')
-
+    print('Warning, sound disabled')
