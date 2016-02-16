050d233afe2d8d38101bb6f9b01cc96c85a29ea9
Romain Claret <contact@rocla.ch>
2016-02-16 20:07:38 +0100

    coming back after a long break...

--- Claret_Visinand.py
+++ Claret_Visinand.py
@@ -3,6 +3,7 @@ import os, sys
 import pygame
 from pygame.locals import *
 import LittleCity
+import geneticAlgoritm as ga
 
 if not pygame.font:
     print('Warning, fonts disabled')
@@ -14,5 +15,14 @@ if __name__ == "__main__":
     filename_cities = "data/pb010.txt"
 
     cities = LittleCity.get_cities(filename_cities)
+    #print(cities)
 
-    print(cities)
+    population_count = 50
+
+    travel_lengths, best_route = ga.find_trip(cities, population_count, ga.use_roulette_selection, ga.improved_greedy_crossover)
+
+    print("Genetic Algorithm")
+    print("=================")
+    print(" - Roulette Selection")
+    print(best_route)
+    print(travel_lengths)
---
+++ geneticAlgoritm.py
@@ -0,0 +1,25 @@
+
+
+class GeneticAlgoritm:
+    def __init__(self):
+        print("Hello I am constructed :)")
+
+def find_trip(cities, population_count, selection_type, crossover_type):
+    travel_lengths = 0
+    best_route = [0]
+    return travel_lengths, best_route
+
+def use_roulette_selection():
+    print("use_roulette_selection")
+
+def improved_greedy_crossover():
+    print("improved_greedy_crossover")
+    for x in range(10):
+        print(x)
+    chomosome_father = []
+
+
+if __name__ == "__main__":
+
+    test = GeneticAlgoritm()
+    improved_greedy_crossover()
